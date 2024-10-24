7eacf5d888c91bc7b812670a1f2168ab0dff0ba0
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-10-24 16:55:51 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,23 @@ from statsmodels.tsa.holtwinters import ExponentialSmoothing
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
 
+
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Título de la aplicación
 st.title('Pronóstico de Holt-Winters Multiplicativo')
 
